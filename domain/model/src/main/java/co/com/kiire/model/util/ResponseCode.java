cbd777ebac18d3f54c769d71f3398d3e3034c124
leforero4-ui <54160303+leforero4-ui@users.noreply.github.com>
2022-08-01 00:13:00 -0500

    se ajusta estructura

--- domain/model/src/main/java/co/com/kiire/model/util/ResponseCode.java
+++ domain/model/src/main/java/co/com/kiire/model/util/ResponseCode.java
@@ -4,10 +4,10 @@ package co.com.kiire.model.util;
  * Enumeracion con el mapeo de los códigos de respuesta
  */
 public enum ResponseCode {
-    KAUS000(500 , "Ocurrió un error inesperado, por favor intenta mas tarde."),
-    KAUS001(200 , "Operación exitosa."),
-    KAUS002(400 , "Campo {0} es obligatorio."),
-    KAUS003(403 , "Usuario se encuentra en lista restrictiva.");
+    KAUS000(500, "Ocurrió un error inesperado, por favor intenta mas tarde."),
+    KAUS001(200, "Operación exitosa."),
+    KAUS002(400, "Campo {0} es obligatorio."),
+    KAUS003(403, "Usuario se encuentra en lista restrictiva.");
 
     private final int status;
     private final String htmlMessage;
