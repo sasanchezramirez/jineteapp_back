6ff4df6ab082d6cc9c471772f9d9cb92c7a13ed0
leforero4-ui <54160303+leforero4-ui@users.noreply.github.com>
2022-07-31 23:40:57 -0500

    se ajusta estructura

--- domain/model/src/main/java/co/com/kiire/model/error/CustomException.java
+++ domain/model/src/main/java/co/com/kiire/model/error/CustomException.java
@@ -7,12 +7,9 @@ import java.text.MessageFormat;
 
 public class CustomException extends RuntimeException {
 
-    /** Serial version */
     @Serial
     private static final long serialVersionUID = 3508567824775716466L;
-    /**
-     * Valor que permite definir un estado para la respuesta HTTP en caso de que deba ser propagada desde el origen de la excepcion
-     */
+
     private final ResponseCode responseCode;
 
     public CustomException(ResponseCode responseCode, String... params) {
