b584784966aa442b9c1028c68a3592f54009dbb3
leforero4-ui <54160303+leforero4-ui@users.noreply.github.com>
2022-08-09 12:06:12 -0500

    se ajusta estructura

--- domain/model/src/main/java/co/com/kiire/model/FieldError.java
+++ domain/model/src/main/java/co/com/kiire/model/FieldError.java
@@ -3,9 +3,16 @@ package co.com.kiire.model;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 @Getter
 @RequiredArgsConstructor
-public class FieldError {
+public class FieldError implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 3708567824775716466L;
+
     private final String field;
     private final String error;
 }
